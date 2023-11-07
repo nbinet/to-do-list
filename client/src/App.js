7751c32ea21d226065815b0b9beef6bbcca23f79
nbinet <nathan.binet99@gmail.com>
2023-11-07 23:05:45 +0100

    add MainComponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,21 @@
-export default function App() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import MainComponent from "./MainComponent";
+
+function App() {
+    return (
+        <Router>
+            <Fragment>
+                <header className="">
+                    <div>To-do list little app</div>
+                    <Link to="/">Home</Link>
+                </header>
+                <div className="main">
+                    <Route exact path="/" component={MainComponent} />
+                </div>
+            </Fragment>
+        </Router>
+    );
 }
+
+export default App;
---
+++ client/src/MainComponent.js
@@ -0,0 +1,58 @@
+import { useCallback, useState, useEffect } from "react";
+import axios from "axios";
+
+const MainComponent = () => {
+    const [values, setValues] = useState([]);
+    const [value, setValue] = useState("");
+
+    const getAllNumbers = useCallback(async () => {
+        // we will use nginx to redirect it to the proper URL
+        const data = await axios.get("/api/values/all");
+        setValues(data.data.rows.map((row) => row.number));
+    }, []);
+
+    const saveNumber = useCallback(
+        async (event) => {
+            event.preventDefault();
+
+            await axios.post("/api/values", {
+                value,
+            });
+
+            setValue("");
+            getAllNumbers();
+        },
+        [value, getAllNumbers]
+    );
+
+    useEffect(() => {
+        getAllNumbers();
+    }, []);
+
+    return (
+        <div className="bg-indigo-500">
+            <button onClick={getAllNumbers}>Get all numbers</button>
+            <br />
+            <span className="title">Values</span>
+            <div className="values">
+                {values.map((value, index) => (
+                    <div key={index} className="value">
+                        {value}
+                    </div>
+                ))}
+            </div>
+            <form className="form" onSubmit={saveNumber}>
+                <label>Enter your value: </label>
+                <input
+                    value={value}
+                    onChange={(event) => {
+                        setValue(event.target.value);
+                    }}
+                />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default MainComponent;
